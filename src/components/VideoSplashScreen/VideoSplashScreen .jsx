d7f00957b3a0f0cdf9c6ae8508328bc648d86a22
alirezahs.it <alirezahs.it@gmail.com>
2025-03-13 13:59:03 +0330

    Fix video sizes

--- src/components/VideoSplashScreen/VideoSplashScreen .jsx
+++ src/components/VideoSplashScreen/VideoSplashScreen .jsx
@@ -14,15 +14,18 @@ const VideoSplashScreen = ({ onFinish }) => {
   }, [onFinish]);
 
   return (
+      <div>
     <video
       autoPlay
       playsInline
       muted
       className="object-cover"
-      style={{ width: "20rem", height: "60rem", objectFit: "cover" }}
+      style={{ width: "100%", height: "100%", paddingTop: "15rem",paddingBottom: "5rem", objectFit: "cover" }}
     >
       <source src={sampleVideo} type="video/mp4" />
     </video>
+   <h5>The Best Game in the World!</h5>
+</div>
   );
 };
 
